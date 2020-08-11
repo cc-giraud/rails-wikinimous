9eaa364dfd86427fb45d4be97c41aca6d91792d5
chrystèle claude giraud <giraud@therapose-formations.com>
2020-08-11 16:55:49 +0200

    end of the day

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::Base
+def index
+
+end
+
+def show
+
+end
+
+def new
+
+end
+
+def create
+
+end
+
+def update
+
+end
+
+
+def delete
+
+end
+
+def method_name
+
+end
+
+
 end
